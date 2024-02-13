629cdad1d7a461dfdbb148b6e8234db41a15d79a
Sebastian Ismael <sebastianismael@gmail.com>
2024-02-13 10:21:33 +0100

    convert repositories

---
+++ src/main/java/ar/edu/unlam/tallerweb1/infrastructure/UserJdbcRepostory.kt
@@ -0,0 +1,41 @@
+package ar.edu.unlam.tallerweb1.infrastructure
+
+import ar.edu.unlam.tallerweb1.domain.UserRepository
+import ar.edu.unlam.tallerweb1.domain.model.FriendsGroup
+import ar.edu.unlam.tallerweb1.domain.model.User
+import ar.edu.unlam.tallerweb1.infrastructure.utils.DataSource
+import ar.edu.unlam.tallerweb1.infrastructure.utils.JdbcUtil.*
+import java.sql.Connection
+
+class UserJdbcRepostory(dataSource: DataSource) : JdbcRepository(dataSource), UserRepository {
+    override fun save(user: User) =
+        executeInTransaction { connection: Connection ->
+            val sql = "INSERT INTO user (name) VALUES (?)"
+            val ps = prepareStatement(connection, sql)
+            setString(1, user.name, ps)
+            execute(ps)
+        }
+
+    override fun saveWithFriendGroup(user: User) =
+            executeInTransaction { connection: Connection ->
+            val sql = "INSERT INTO user (name, friends_group_id) VALUES (?,?)"
+            val ps = prepareStatement(connection, sql)
+            setString(1, user.name, ps)
+            setLong(2, user.friendsGroup.id, ps)
+            execute(ps)
+        }
+
+    override fun findByName(name: String) =
+        findInTransaction({ connection: Connection, username: String ->
+            val sql = "select * from user where name = ?"
+            val ps = prepareStatement(connection, sql)
+            setString(1, username, ps)
+            val users = executeQuery(ps)
+            if (next(users)) {
+                val user = User(getLong("id", users), getString("name", users))
+                user.friendsGroup = FriendsGroup(getLong("friends_group_id", users), "")
+                return@findInTransaction user
+            }
+            return@findInTransaction null
+        }, name)
+}
