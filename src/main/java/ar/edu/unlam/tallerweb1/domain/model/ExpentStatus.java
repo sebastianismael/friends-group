c7177c95c268414a9a3e519eb5df527cd0a8498f
Sebastian Ismael <sebastianismael@gmail.com>
2024-02-13 11:57:36 +0100

    convert entities

--- src/main/java/ar/edu/unlam/tallerweb1/domain/model/ExpentStatus.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unlam.tallerweb1.domain.model;
-
-public enum ExpentStatus {
-    OPEN, CLOSED;
-}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/model/ExpentStatus.kt
@@ -0,0 +1,5 @@
+package ar.edu.unlam.tallerweb1.domain.model
+
+enum class ExpentStatus {
+    OPEN, CLOSED
+}
