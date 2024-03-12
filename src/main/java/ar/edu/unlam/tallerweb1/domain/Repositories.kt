183002213aabfadadd42edd5c1ca124ca13f1ff9
Sebastian Ismael <sebastianismael@gmail.com>
2024-03-12 13:57:42 +0100

    repositories as singletons

--- src/main/java/ar/edu/unlam/tallerweb1/domain/Repositories.kt
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/Repositories.kt
@@ -4,35 +4,49 @@ import ar.edu.unlam.tallerweb1.infrastructure.utils.DataSource
 import ar.edu.unlam.tallerweb1.infrastructure.utils.MySqlDataSource
 import org.reflections.Reflections
 import org.reflections.scanners.SubTypesScanner
+import kotlin.reflect.KClass
 
 object Repositories {
 
-    fun getSharedExpensesRepository() =
-        getRepository(SharedExpensesRepository::class.java, MySqlDataSource.instance())
+    private val implementations = mutableMapOf<KClass<out Any>, Any>()
 
-    fun getUserRepository() =
-        getRepository(UserRepository::class.java, MySqlDataSource.instance())
+    fun getSharedExpensesRepository(): SharedExpensesRepository =
+        getImplementation(SharedExpensesRepository::class, MySqlDataSource.instance()) as SharedExpensesRepository
 
-    fun getFriendsGroupRepository() =
-        getRepository(FriendsGroupRepository::class.java, MySqlDataSource.instance())
+    fun getUserRepository(): UserRepository =
+        getImplementation(UserRepository::class, MySqlDataSource.instance()) as UserRepository
 
-    fun getPaymentRepository() =
-        getRepository(PaymentRepository::class.java, MySqlDataSource.instance())
+    fun getFriendsGroupRepository(): FriendsGroupRepository =
+        getImplementation(FriendsGroupRepository::class, MySqlDataSource.instance()) as FriendsGroupRepository
+
+    fun getPaymentRepository(): PaymentRepository =
+        getImplementation(PaymentRepository::class, MySqlDataSource.instance()) as PaymentRepository
 
     // TODO mecanismo para proveer las implementaciones de los repositorios que estan en infraestructura.
     //  * mejora #1: usa una libreria externa, no enccontre un mecanismo nativo en java para conocer las clases que
     //  implementan una interface, buscar alguna forma nativa de resolverlo
-    //  * mejora #2: manejar los repositorios como singleton, ya que estamos instanciando el repo cada vez.
+    //  * mejora #2: manejar los repositorios como singleton, ya que estamos instanciando el repo cada vez. DONE!!!!
     //  * mejora #3: no soporta mas de una implementacion de la interface
-    private fun <T> getRepository(interfaze: Class<T>, dataSource: MySqlDataSource): T {
-        val reflections = Reflections("ar.edu.unlam.tallerweb1.infrastructure", SubTypesScanner(false))
-        val implementations = reflections.getSubTypesOf(interfaze)
-        if (implementations.isEmpty()) throw RuntimeException("$interfaze hasn't implementations!!!")
+    private fun <T : Any> getImplementation(repository: KClass<T>, dataSource: MySqlDataSource): Any? {
         try {
-            return implementations.iterator().next().getConstructor(DataSource::class.java).newInstance(dataSource)
+            if(!this.implementations.contains(repository))
+                this.implementations[repository] = getSubclassOf(repository).instantiate(dataSource)
+            return this.implementations[repository]
         } catch (e: Exception) {
-            throw RuntimeException("$interfaze implementation can't be instantiated: ", e)
+            throw RuntimeException("$repository implementation can't be instantiated: ", e)
         }
     }
 
+    private fun <T : Any> getSubclassOf(repository: KClass<T>): Class<out T> {
+        val reflections = Reflections("ar.edu.unlam.tallerweb1.infrastructure", SubTypesScanner(false))
+        val implementations = reflections.getSubTypesOf(repository.java)
+        if (implementations.isEmpty()) throw RuntimeException("$repository hasn't implementations!!!")
+        return implementations.iterator().next()
+    }
+
+    private fun <T : Any> Class<T>.instantiate(dataSource: MySqlDataSource) =
+        this.getConstructor(DataSource::class.java).newInstance(dataSource)
+
 }
+
+
